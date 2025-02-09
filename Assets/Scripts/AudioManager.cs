8b16a3eed33c928785e79eb9e1a6cebc8bc80fb8
MilesRogers3D <130790083+MilesRogers3D@users.noreply.github.com>
2025-02-09 15:13:56 -0600

    Song +  AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    public float loopPoint = 0.0F;
+    
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.Play();
+    }
+
+    void Update()
+    {
+        if (Mathf.Approximately(
+                _audioSource.time,
+                _audioSource.clip.length))
+        {
+            _audioSource.Pause();
+            _audioSource.time = loopPoint;
+            _audioSource.Play();
+        }
+    }
+}
