5089ded309a2dad0aa00937b9c35702a4ef9e12e
Trinity <thutson3876@gmail.com>
2025-02-09 14:39:37 -0600

    Level3

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class LookAtPlayer : MonoBehaviour
 {
     [SerializeField] private Transform player;
+
+    private void Awake()
+    {
+        if(player == null)
+        {
+            player = Camera.main.transform;
+        }
+    }
+
     void Update()
     {
         transform.LookAt(player.position);
