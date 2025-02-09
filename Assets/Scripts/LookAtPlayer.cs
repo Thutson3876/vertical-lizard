6354d94865b1e00d352db4d5773adde8331b3ac4
MilesRogers3D <130790083+MilesRogers3D@users.noreply.github.com>
2025-02-09 13:17:55 -0600

    Terlet

---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    void Update()
+    {
+        transform.LookAt(player.position);
+    }
+}
